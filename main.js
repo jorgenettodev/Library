13b00333f72404b847b4481fa37abf51eb89fab6
Jorge Netto <jorgenetto96@gmail.com>
2024-01-04 05:39:33 -0300

    Create test functions
    - add new books.
    - display all books.

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const myLibrary = [];
 
-let Book = function(title, author, pages, readStatus) {
+function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.readStatus = readStatus;
 
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages`;
+        return `${this.title} by ${this.author}, ${this.pages} pages. Reading status: ${this.readStatus}.`;
     };
+
+    this.status = function() {
+        return `Reading status: ${readStatus}`;
+    }
 };
 
-let book1 = new Book('O Cavaleiro preso na armadura','Robert Fisher', 62, 'Reading currently');
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
 
-console.log(book1.info());
+// create some books to test it.
+function testBooks() {
+    let book1 = new Book('O Cavaleiro preso na armadura','Robert Fisher', 62, 'reading');
+    // add book1 to library
+    addBookToLibrary(book1);
+
+    let book2 = new Book('The war of the worlds', 'H. G. Wells', 450, 'reading')
+    addBookToLibrary(book2);
+
+
+}
+
+// show all books in the myLibrary array.
+function showAllBooks() {
+    // loop through the array and console.log all books
+    for (const key in myLibrary) {
+        console.log(`Book name: ${myLibrary[key].info()}`);
+    }
+}
