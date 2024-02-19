714f459ca5e896085775c1bc3eeb8a59c898127d
Jorge Netto <jorgenetto96@gmail.com>
2024-02-19 16:09:25 -0300

    Create function to add new books to the books__grid, but still with mockup books.

--- script.js
+++ script.js
@@ -40,7 +40,56 @@ function showAllBooks() {
 }
 
 // btn add
-const btnAdd = document.querySelector('#btn__add');
-btnAdd.addEventListener('click', (e) => {
+const btnAddBook = document.querySelector('#btn__add');
+btnAddBook.addEventListener('click', (e) => {
     console.log('clicked');
 });
+
+// books__grid
+const booksGrid = document.querySelector('.books__grid');
+
+function createBook() {
+    let newBook = document.createElement('div');
+    newBook.classList.add('book__card');
+
+
+    const bookTitle = document.createElement('h2')
+    bookTitle.classList.add('book__title');
+    bookTitle.innerText = 'Bazinga';
+
+    const bookAuthor = document.createElement('p')
+    bookAuthor.classList.add('book__author');
+    bookAuthor.innerText = 'Bazinga Author';
+
+    const bookPages = document.createElement('p')
+    bookPages.classList.add('book__pages');
+    bookPages.innerText = '459 Pages';
+
+    
+
+    // book buttons
+
+    const btnReadStatus = document.createElement('button');
+    btnReadStatus.innerText = 'Already read'
+    btnReadStatus.classList.add('btn__readStatus');
+    btnReadStatus.classList.add('btn');
+
+
+    const btnDeleteBook = document.createElement('button');
+    btnDeleteBook.classList.add('btn');
+    btnDeleteBook.classList.add('btn__delete');
+    btnDeleteBook.innerText = 'delete';
+
+    
+    newBook.appendChild(bookTitle);
+    newBook.appendChild(bookAuthor);
+    newBook.appendChild(bookPages);
+    newBook.appendChild(btnReadStatus);
+    newBook.appendChild(btnDeleteBook);
+
+    booksGrid.appendChild(newBook);
+
+    
+}
+
+createBook();
