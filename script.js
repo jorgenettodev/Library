fc26cf69aa6612b7390ced573422ddc529c87073
jorgenettodev <jorgenetto96@gmail.com>
2024-03-04 20:06:06 -0300

    merge modal

--- script.js
+++ script.js
@@ -20,28 +20,6 @@ function Book(title, author, pages, readStatus) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
-
-// create some books to test it.
-function testBooks() {
-    let book1 = new Book('O Cavaleiro preso na armadura','Robert Fisher', 62, 'already read');
-    // add book1 to library
-    addBookToLibrary(book1);
-
-    let book2 = new Book('The war of the worlds', 'H. G. Wells', 450, 'reading')
-    addBookToLibrary(book2);
-
-
-}
-
-// show all books in the myLibrary array.
-function showAllBooks() {
-    // loop through the array and console.log all books
-    for (const key in myLibrary) {
-        console.log(`Book name: ${myLibrary[key].info()}`);
-    }
-}
-
-
 // books__grid
 const booksGrid = document.querySelector('.books__grid');
 
